5ff8c4bc2e48bcb598d5a66e690f3be85dc46412
Frank Terbeck <ft@bewatermyfriend.org>
2014-02-15 13:00:51 +0100

    Use the "-s" switch when calling scheme scripts
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- gen-gps.sh
+++ gen-gps.sh
@@ -3,4 +3,4 @@
 GUILE_AUTO_COMPILE=0
 export GUILE_AUTO_COMPILE
 
-exec ${GUILE_BINARY:-guile} ./gen-gps.scm
+exec ${GUILE_BINARY:-guile} -s ./gen-gps.scm
--- test-this-terminal.sh
+++ test-this-terminal.sh
@@ -6,4 +6,4 @@ export GUILE_LOAD_PATH
 GUILE_AUTO_COMPILE=0
 export GUILE_AUTO_COMPILE
 
-exec ${GUILE_BINARY:-guile} ./test-this-terminal.scm
+exec ${GUILE_BINARY:-guile} -s ./test-this-terminal.scm

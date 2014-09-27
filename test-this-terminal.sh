8fa03e44dc8e5e62987657e31fdcdd56d743f00f
Frank Terbeck <ft@bewatermyfriend.org>
2014-09-27 11:28:44 +0200

    Add message to test-this-terminal.sh
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- test-this-terminal.sh
+++ test-this-terminal.sh
@@ -9,6 +9,9 @@ export GUILE_LOAD_COMPILED_PATH
 GUILE_AUTO_COMPILE=0
 export GUILE_AUTO_COMPILE
 
+printf 'Running general module plausibility test...\n'
 for i in test-this-terminal*.scm; do
-    ${GUILE_BINARY:-guile} -s "$i"
+    ${GUILE_BINARY:-guile} -s "$i" || exit 1
 done
+printf 'Passed general module plausibility test.\n'
+exit 0

995fcffff914f2b67156350d0546f04cb9a326e1
Frank Terbeck <ft@bewatermyfriend.org>
2014-10-18 16:36:05 +0200

    version: use +git suffix
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/termios.scm
+++ scheme/termios.scm
@@ -35,7 +35,7 @@
             termios-failure?
             termios-version))
 
-(define termios-version "0.5")
+(define termios-version "0.5+git")
 
 ;; (dynamic-link "libc") fails on debian, because the "libc.so" file it finds
 ;; it not actually an ELF library but rather a GNU ld script. Without library,
